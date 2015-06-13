893ceef3bc6b8ec506ad6b835f882ebb1b550ceb
mingkim <ming3772@gmail.com>
2015-06-13 16:57:32 +0900

    enrollment page added

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/enrollment', function (req, res, next) {
+    res.render('enrollment');
+});
+
+router.post('/enrollment', function(req, res){
+    var userName = req.body.userName;
+    var html = 'Hello: ' + userName + '.<br>' +
+        '<a href="/">Try again.</a>';
+    res.send(html);
+});
+
 module.exports = router;
